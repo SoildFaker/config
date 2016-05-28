d7b121d046e0366e274c8f6255fb4e7a6da2b955
soildfaker <enbinli@outlook.com>
2016-05-28 15:18:52 +0800

    config script

---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ln -s $PWD/awesome/awesome /home/enbin/.config/awesome
+ln -s $PWD/vim/.vim /home/enbin/.vim
+ln -s $PWD/vim/.vimrc /home/enbin/.vimrc
+ln -s $PWD/urxvt/.Xdefaults /home/enbin/.Xdefaults
+ln -s $PWD/xinit/.xinitrc /home/enbin/.xinitrc
+ln -s $PWD/xinit/.xprofile /home/enbin/.xprofile
+ln -s $PWD/xinit/.xserverrc /home/enbin/.xserverrc
