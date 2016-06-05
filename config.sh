ca90dc82ba0f63349e49a2ff692c1ae607fc535f
soildfaker <enbinli@outlook.com>
2016-06-05 15:01:24 +0800

    change script

--- config.sh
+++ config.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-ln -s $PWD/awesome/awesome /home/enbin/.config/awesome
+rm -r /home/enbin/.vim
+rm -r /home/enbin/.vimrc
+rm -r /home/enbin/.Xdefaults
+rm -r /home/enbin/.xinitrc
+rm -r /home/enbin/.xprofile
+rm -r /home/enbin/.xserverrc
+rm -r /home/enbin/.config/openbox
+rm -r /home/enbin/.config/awesome
+ln -s $PWD/awesome /home/enbin/.config/awesome
 ln -s $PWD/vim/.vim /home/enbin/.vim
 ln -s $PWD/vim/.vimrc /home/enbin/.vimrc
 ln -s $PWD/urxvt/.Xdefaults /home/enbin/.Xdefaults
 ln -s $PWD/xinit/.xinitrc /home/enbin/.xinitrc
 ln -s $PWD/xinit/.xprofile /home/enbin/.xprofile
 ln -s $PWD/xinit/.xserverrc /home/enbin/.xserverrc
+ln -s $PWD/openbox /home/enbin/.config/openbox
