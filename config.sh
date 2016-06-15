c9474271f9c3a404a75ada2a28afe1c2fca09601
soildfaker <enbinli@outlook.com>
2016-06-15 19:52:58 +0800

    midify script

--- config.sh
+++ config.sh
@@ -5,6 +5,7 @@ rm -r /home/enbin/.Xdefaults
 rm -r /home/enbin/.xinitrc
 rm -r /home/enbin/.xprofile
 rm -r /home/enbin/.xserverrc
+rm -r /home/enbin/.tmux.conf
 rm -r /home/enbin/.config/openbox
 rm -r /home/enbin/.config/awesome
 ln -s $PWD/awesome /home/enbin/.config/awesome
@@ -15,3 +16,4 @@ ln -s $PWD/xinit/.xinitrc /home/enbin/.xinitrc
 ln -s $PWD/xinit/.xprofile /home/enbin/.xprofile
 ln -s $PWD/xinit/.xserverrc /home/enbin/.xserverrc
 ln -s $PWD/openbox /home/enbin/.config/openbox
+ln -s $PWD/tmux/.tmux.conf
